571a986c0d7d47448d5a3a83b47d6072a6e3af11
Loisinho <159461422+loisinho87@users.noreply.github.com>
2024-05-15 10:08:36 +0000

    ExcusasLois

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  function generandoexcusa() {
+    let quien = ["Mi perro", "Mi abuela", "El cartero", "Mi pájaro"];
+    let accion = ["se comió", "orinó en", "rompió", "aplastó"];
+    let que = ["mi tarea", "mi teléfono", "el carro"];
+    let cuando = [
+      "antes de la clase",
+      "mientras dormía",
+      "mientras hacía ejercicio",
+      "durante el almuerzo",
+      "mientras rezaba"
+    ];
+
+    let quienrandom = quien[Math.floor(Math.random() * quien.length)]; //Math.random() devuelve un número decimal aleatorio entre 0 (inclusive) y 1 (exclusivo)
+    let accionrandom = accion[Math.floor(Math.random() * accion.length)]; //un índice aleatorio válido, debo * Math.random() por la longitud del array
+    let querandom = que[Math.floor(Math.random() * que.length)]; //utilizo Math.floor() para redondear hacia abajo o arriba el número resultante.
+    let cuandorandom = cuando[Math.floor(Math.random() * cuando.length)]; // lo que significa que seleccionaremos el elemento en la posición ? del array.
+
+    let excusa = `${quienrandom} ${accionrandom} ${querandom} ${cuandorandom}`; // ${} para concatenar
+    // Actualizamos el contenido de la etiqueta con la nueva excusa
+    document.getElementById("excusalois").innerHTML = excusa; // document.getElementById() método predeterminado en JavaScript que se utiliza para seleccionar un elemento HTML en función de su ID
+    //.innerHTML es una propiedad incorporada en JavaScript que se utiliza comúnmente para manipular el contenido HTML de los elementos seleccionados.
+  }
+
+  generandoexcusa(); // Llamamos a la función generandoexcusa() para que se ejecute cuando la página se cargue completamente
   console.log("Hello Rigo from the console!");
 };
