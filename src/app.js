00d7e68291d9e2741be7c3ee92c4971c794682ab
Loisinho <159461422+loisinho87@users.noreply.github.com>
2024-05-17 09:37:52 +0000

    nueva actualización

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  function generandoexcusa() {
+  function generandoExcusa() {
     let quien = ["Mi perro", "Mi abuela", "El cartero", "Mi pájaro"];
     let accion = ["se comió", "orinó en", "rompió", "aplastó"];
     let que = ["mi tarea", "mi teléfono", "el carro"];
@@ -19,17 +19,18 @@ window.onload = function() {
       "mientras rezaba"
     ];
 
-    let quienrandom = quien[Math.floor(Math.random() * quien.length)]; //Math.random() devuelve un número decimal aleatorio entre 0 (inclusive) y 1 (exclusivo)
-    let accionrandom = accion[Math.floor(Math.random() * accion.length)]; //un índice aleatorio válido, debo * Math.random() por la longitud del array
-    let querandom = que[Math.floor(Math.random() * que.length)]; //utilizo Math.floor() para redondear hacia abajo o arriba el número resultante.
-    let cuandorandom = cuando[Math.floor(Math.random() * cuando.length)]; // lo que significa que seleccionaremos el elemento en la posición ? del array.
+    let quienRandom = quien[Math.floor(Math.random() * quien.length)]; //Math.random() devuelve un número decimal aleatorio entre 0 (inclusive) y 1 (exclusivo)
+    let accionRandom = accion[Math.floor(Math.random() * accion.length)]; //un índice aleatorio válido, debo * Math.random() por la longitud del array
+    let queRandom = que[Math.floor(Math.random() * que.length)]; //utilizo Math.floor() para redondear hacia abajo o arriba el número resultante.
+    let cuandoRandom = cuando[Math.floor(Math.random() * cuando.length)]; // lo que significa que seleccionaremos el elemento en la posición ? del array.
 
-    let excusa = `${quienrandom} ${accionrandom} ${querandom} ${cuandorandom}`; // ${} para concatenar
+    // ${} para concatenar variables
     // Actualizamos el contenido de la etiqueta con la nueva excusa
-    document.getElementById("excusalois").innerHTML = excusa; // document.getElementById() método predeterminado en JavaScript que se utiliza para seleccionar un elemento HTML en función de su ID
+
+    document.getElementById(
+      "excusaLois"
+    ).innerHTML = `${quienRandom} ${accionRandom} ${queRandom} ${cuandoRandom}`; // document.getElementById() método predeterminado en JavaScript que se utiliza para seleccionar un elemento HTML en función de su ID
     //.innerHTML es una propiedad incorporada en JavaScript que se utiliza comúnmente para manipular el contenido HTML de los elementos seleccionados.
   }
-
-  generandoexcusa(); // Llamamos a la función generandoexcusa() para que se ejecute cuando la página se cargue completamente
-  console.log("Hello Rigo from the console!");
+  generandoExcusa(); // Llama a la función generandoExcusa() para que se ejecute cuando la página se cargue completamente
 };
